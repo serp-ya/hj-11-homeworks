823a9ae2a5532873fed37c46c91bb570de595984
serp-ya <sergei.kundryukov1989@gmail.com>
2017-07-24 22:11:40 +0300

    Complited teacher's comments

--- dom/shop/js/main.js
+++ dom/shop/js/main.js
@@ -1,14 +1,15 @@
 const addButtons = document.querySelectorAll('button.add');
 const cartCount = document.getElementById('cart-count');
 const cartTotalPrice = document.getElementById('cart-total-price');
+let goodsCount = parseInt(cartCount.innerText);
+let goodsTotalPrice = parseInt(cartTotalPrice.innerText);
 
-function addGoods() {
-    let goodsCount = parseInt(cartCount.innerText);
-    let goodsTotalPrice = parseInt(cartTotalPrice.innerText);
-    let thisPrice = parseInt(this.dataset.price);
+function addGoods(event) {
+    goodsCount += 1;
+    goodsTotalPrice += parseInt(event.target.dataset.price);
 
-    cartCount.innerHTML = ++goodsCount;
-    cartTotalPrice.innerHTML = goodsTotalPrice + thisPrice;
+    cartCount.innerHTML = goodsCount;
+    cartTotalPrice.innerHTML = getPriceFormatted(goodsTotalPrice);
 }
 
 for (let btn of addButtons) {
