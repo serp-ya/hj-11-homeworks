101e22f5bb973c517d6516a4b2a329199c5e3d32
serp-ya <sergei.kundryukov1989@gmail.com>
2017-07-27 10:25:22 +0300

    Made delegation in event listener handler

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,20 +1,15 @@
 'use strict';
-const tableHeaders = document.getElementsByTagName('th');
-
 function handleTableClick(event) {
-  let targetData = event.target.dataset;
-  let sortOrder = targetData.propName;
+  if (event.target.tagName === 'TH') {
+    let targetData = event.target.dataset;
 
-  if (!targetData.dir) {
+    if (!targetData.dir) {
       targetData.dir = 1;
-  } else {
+    } else {
       targetData.dir *= -1;
-  }
-  event.stopPropagation();
-  table.dataset.sortBy = sortOrder;
-  sortTable(sortOrder, targetData.dir);
-}
+    }
 
-for (let header of tableHeaders) {
-    header.addEventListener('click', handleTableClick)
+    table.dataset.sortBy = targetData.propName;
+    sortTable(targetData.propName, targetData.dir);
+  }
 }
