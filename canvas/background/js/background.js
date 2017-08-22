e1700f25026a13a91d4e083a651c8a3d5763213f
serp-ya <sergei.kundryukov1989@gmail.com>
2017-08-23 01:26:56 +0300

    Changed some things

--- canvas/background/js/background.js
+++ canvas/background/js/background.js
@@ -28,9 +28,9 @@ class Circle {
     this.size = getRandom(0.1, 0.6);
     this.outline = 5 * this.size;
     this.radius = 12 * this.size;
-    this.motion = timeFunctions[Math.round(getRandom(0, 1))];
     this.vector = 1;
     this.speed = getRandom(1, 5);
+    this.motion = timeFunctions[Math.round(Math.random())];
   }
 }
 
@@ -39,23 +39,27 @@ class Cross {
     this.x = x;
     this.y = y;
     this.size = getRandom(0.1, 0.6);
-    this.side = 20 * this.size;
     this.outline = 5 * this.size;
+    this.side = 20 * this.size;
     this.angle = Math.round(getRandom(0, 360));
     this.rotationSpeed = getRandom(-0.2, 0.2);
-    this.motion = timeFunctions[Math.round(Math.random())];
     this.vector = 1;
     this.speed = getRandom(1, 5);
+    this.motion = timeFunctions[Math.round(Math.random())];
   }
 }
 
 function createFigures(amountFrom, amountTo) {
   for (let i = 0; i < getRandom(amountFrom, amountTo); i++) {
-    figures.push(new Cross(Math.round(getRandom(0, canvas.width)), Math.round(getRandom(0, canvas.height))))
+    figures.push(
+      new Cross(Math.round(getRandom(0, canvas.width)), Math.round(getRandom(0, canvas.height)))
+    );
   }
 
   for (let i = 0; i < getRandom(amountFrom, amountTo); i++) {
-    figures.push(new Circle(Math.round(getRandom(0, canvas.width)), Math.round(getRandom(0, canvas.height))))
+    figures.push(
+      new Circle(Math.round(getRandom(0, canvas.width)), Math.round(getRandom(0, canvas.height)))
+    );
   }
 }
 
@@ -132,7 +136,7 @@ function tick() {
     } else {
       drawCircle(figure);
     }
-  })
+  });
 }
 
 createFigures(80, 150);
